4729f75bc25e264111c2a668bd62cb72a3eed199
Caio Tonetti <caio.tonetti@gmail.com>
2026-03-02 20:52:22 +0000

    Persist hichi devbox SSH key access and import hardware profile

--- hosts/hichi/default.nix
+++ hosts/hichi/default.nix
@@ -2,7 +2,20 @@
 {
   imports = [
     ../devbox/default.nix
+    ./hardware-configuration.nix
   ];
 
   networking.hostName = "hichi-devbox";
+
+  users.users.lionswrath.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1OTY109TkeFKYgv2mHKGqTNvh6+LQhOKDsUW6VdRw4 ragost@orochi-client"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9wPZ+cAFB6nFo3QjDD49encZWzqlIjt3Sf0nZ99KYH caio.tonetti@gmail.com"
+  ];
+
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+    PubkeyAuthentication = true;
+    PermitRootLogin = "no";
+  };
 }
